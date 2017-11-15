dc2161c12201395cd0ae1f985d9e6172a02afe76
David Skoglund <dskoggz@gmail.com>
2017-11-15 14:09:33 -0800

    #8. API endpoint created for current tournament field.

--- index.js
+++ index.js
@@ -121,75 +121,75 @@ function getCurrentField(URL) {
     })
   });
 }
-app.get('/pga_leaderboard', function(req, res){
+// app.get('/pga_leaderboard', function(req, res){
+//
+//   request(leaderboardUrl, function(error, response, html){
+//     if(!error){
+//
+//       const $ = cheerio.load(html);
+//
+//       const leaderBoard = []
+//
+//       $("table tr").filter(function(){
+//         const data = $(this);
+//         const player = { position: '', playerName: '', overallScore: '', roundScore: '', playerUrl: ''}
+//         const hasNumber = /\d/;
+//         if (hasNumber.test(data.children().first().text())) {
+//           player.position = data.children().first().text()
+//           player.playerName = data.children("td:nth-child(2)").text()
+//           player.overallScore = data.children("td:nth-child(3)").text()
+//           player.roundScore = data.children("td:nth-child(4)").text()
+//           player.playerUrl = "http://m.espn.com/golf/"+data.children("td:nth-child(2)").children().attr('href')
+//         }
+//         if (hasNumber.test(player.position)) {
+//           leaderBoard.push(player)
+//         }
+//         console.log(leaderBoard)
+//       })
+//     }
+//     else {
+//       console.log('No tournament')
+//     }
+//
+//   res.send('leaderboard data in console')
+//   });
+// });
 
-  request(leaderboardUrl, function(error, response, html){
-    if(!error){
-
-      const $ = cheerio.load(html);
-
-      const leaderBoard = []
-
-      $("table tr").filter(function(){
-        const data = $(this);
-        const player = { position: '', playerName: '', overallScore: '', roundScore: '', playerUrl: ''}
-        const hasNumber = /\d/;
-        if (hasNumber.test(data.children().first().text())) {
-          player.position = data.children().first().text()
-          player.playerName = data.children("td:nth-child(2)").text()
-          player.overallScore = data.children("td:nth-child(3)").text()
-          player.roundScore = data.children("td:nth-child(4)").text()
-          player.playerUrl = "http://m.espn.com/golf/"+data.children("td:nth-child(2)").children().attr('href')
-        }
-        if (hasNumber.test(player.position)) {
-          leaderBoard.push(player)
-        }
-        console.log(leaderBoard)
-      })
-    }
-    else {
-      console.log('No tournament')
-    }
-
-  res.send('leaderboard data in console')
-  });
-});
-
-app.get('/pga_player_score', function(req, res){
-
-  // let url = req.playerUrl
-  // let round = req.round
-  // const playerUrl = url + round
-  //temporary URL. Will be in the request headers for app.
-  const playerUrl = "http://m.espn.com/golf/playercast?playerId=6931&tournamentId=2699&wjb=&round="+1
-
-  request(playerUrl, function(error, response, html){
-    if(!error){
-
-      const $ = cheerio.load(html);
-
-      const roundScore = []
-
-      $("table tr").filter(function(){
-        const data = $(this);
-        const hole = { hole: '', holePar: '', holeScore: '', roundScore: '', bestBall: ''}
-        const hasNumber = /\d/;
-        if (hasNumber.test(data.children().first().text())) {
-          hole.hole = data.children().first().text()
-          hole.holePar = data.children("td:nth-child(2)").text()
-          hole.holeScore = data.children("td:nth-child(3)").text()
-          hole.roundScore = data.children("td:nth-child(4)").text()
-          hole.bestBall = data.children("td:nth-child(2)").text()-data.children("td:nth-child(3)").text()
-        }
-        if (hasNumber.test(hole.hole)) {
-          roundScore.push(hole)
-        }
-        console.log(roundScore)
-      })
-    }
-  res.send('leaderboard data in console')
-  });
-});
+// app.get('/pga_player_score', function(req, res){
+//
+//   // let url = req.playerUrl
+//   // let round = req.round
+//   // const playerUrl = url + round
+//   //temporary URL. Will be in the request headers for app.
+//   const playerUrl = "http://m.espn.com/golf/playercast?playerId=6931&tournamentId=2699&wjb=&round="+1
+//
+//   request(playerUrl, function(error, response, html){
+//     if(!error){
+//
+//       const $ = cheerio.load(html);
+//
+//       const roundScore = []
+//
+//       $("table tr").filter(function(){
+//         const data = $(this);
+//         const hole = { hole: '', holePar: '', holeScore: '', roundScore: '', bestBall: ''}
+//         const hasNumber = /\d/;
+//         if (hasNumber.test(data.children().first().text())) {
+//           hole.hole = data.children().first().text()
+//           hole.holePar = data.children("td:nth-child(2)").text()
+//           hole.holeScore = data.children("td:nth-child(3)").text()
+//           hole.roundScore = data.children("td:nth-child(4)").text()
+//           hole.bestBall = data.children("td:nth-child(2)").text()-data.children("td:nth-child(3)").text()
+//         }
+//         if (hasNumber.test(hole.hole)) {
+//           roundScore.push(hole)
+//         }
+//         console.log(roundScore)
+//       })
+//     }
+//   res.send('leaderboard data in console')
+//   });
+// });
 
 
 app.listen('8081')
